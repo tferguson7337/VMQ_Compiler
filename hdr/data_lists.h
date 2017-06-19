bd6b556583e5e92e9782f8ec2ca93ea057e10f4f
Thomas Ferguson <erniecat7@comcast.net>
2017-06-19 14:55:16 -0700

    Updating Func list node struct

--- hdr/data_lists.h
+++ hdr/data_lists.h
@@ -34,12 +34,13 @@ typedef struct str_list_node* STR_LIST;
 
 struct func_list_node
 {
-	int return_type;			// Return type of function, as indicated in cpp file (INT OR FLOAT).
-	char* func_name;			// Name of function as found in cpp file.
-	VAR_LIST VMQ_var_list;			// List for tracking func's vars
-	unsigned int VMQ_local_var_count;	// Tracks number of func's local vars (not params). 
+	int return_type;					// Return type of function, as indicated in cpp file (INT OR FLOAT).
+	char* func_name;					// Name of function as found in cpp file.
+	VAR_LIST var_list;					// List for tracking func's vars
+	unsigned int var_count;				// Tracks number of func's local vars (not params). 
+	unsigned int param_count;			// Tracks number of parameters the function takes as an argument.
 	unsigned int VMQ_start_line;		// Notes line number of stack frame creation op (#)
-	unsigned int VMQ_end_line;		// Notes line number of function end (/, or h if main)
+	unsigned int VMQ_end_line;			// Notes line number of function end (/, or h if main)
 	struct func_list_node* next;
 };
 
