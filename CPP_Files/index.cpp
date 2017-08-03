9f0930739066e4c7f9468747b26cd0fdcc664701
Thomas Ferguson <erniecat7@comcast.net>
2017-08-02 20:20:49 -0700

    Adding new cpp file that tests a complex index expression

---
+++ CPP_Files/index.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int add(int x, int y) { return x + y; }
+int sub(int x, int y) { return x - y; }
+int mul(int x, int y) { return x * y; }
+int div(int x, int y) { return x / y; }
+int mod(int x, int y) { return x % y; }
+
+int main()
+{
+    float f_arr[5];
+
+    f_arr[0] = 1.5;
+    f_arr[1] = 7.34;
+    f_arr[2] = 19.5;
+    f_arr[3] = -0.05;
+    f_arr[4] = -127.502;
+
+    cout << "f_arr[mod(sub(add(f_arr[1], f_arr[2]), div(f_arr[2], f_arr[1])), 5)] == "
+	 <<  f_arr[mod(sub(add(f_arr[1], f_arr[2]), div(f_arr[2], f_arr[1])), 5)] << endl;
+}
